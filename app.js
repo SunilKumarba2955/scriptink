aa5246de1a503e021f6e38b2b40cb16c23df6dda
AnuragCool <anuragcooldavkh@gmail.com>
2020-12-12 23:13:15 +0530

    final

--- app.js
+++ app.js
@@ -15,269 +15,267 @@ var firebaseConfig = {
     messagingSenderId: "862615088549",
     appId: "1:862615088549:web:6390609e12b6015a174c4c"
   };
-
-firebase.initializeApp(firebaseConfig);
-
-
-
   
- 
-
-app.set('view engine','ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static(__dirname + '/public'));
-
-
-
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: 'scriptink.writofest.2020@gmail.com',
-           pass: 'writofest2020scriptink'
-       }
-   });
-
-//home route
-app.get("/",function(req,res){
-    res.render("landing");
-})
-
-
-
-
-
-
-
-
-
-const checkRegistrationStart = (callback)=>{
-    var ref = firebase.database().ref("/WritoFest");
-    ref.once('value').then(snap=>{
-        callback(snap.val().registrationstart);
-    })
-}
-
-
-
-const checkParticipants=(email,usn,callback)=>{
-    var message="";
-    var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
-   
-    ref.once('value').then(snap=>{
-        if(snap.val()!=null){
-        snap.forEach(element=>{
-            if(email===element.val().Email || usn===element.val().USN){
-            
-                message="already exists";
-                 return callback(message);
-            }else{
-                return callback(message);
-            }
-        });
-    }else{
-        return callback(message);
-    }
-    })
-}
-
-
-
-app.get("/team",(req,res)=>{
-    res.render("members");
-})
-
-app.get("/event",(req,res)=>{
-    res.render("event");
-})
-
-app.get("/register",(req,res)=>{
-    res.render("register");
-})
-
-app.post("/registerParticipants",(req,res)=>{
-    var messageback="";
-    var name=req.body.name;
-    var usn=req.body.usn;
-    var email=req.body.email;
-    var phone=req.body.phone;
-    var city=req.body.city;
-    var college=req.body.college;
-    var selected=req.body.selected;
-   
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  
     
+   
+  
+  app.set('view engine','ejs');
+  
+  app.use(bodyParser.urlencoded({extended: true}));
+  
+  app.use(express.static(__dirname + '/public'));
+  
+  
+  
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+             user: 'scriptink.writofest.2020@gmail.com',
+             pass: process.env.PASS
+         }
+     });
+  
+  //home route
+  app.get("/",function(req,res){
+      res.render("landing");
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  const checkRegistrationStart = (callback)=>{
+      var ref = firebase.database().ref("/WritoFest");
+      ref.once('value').then(snap=>{
+          callback(snap.val().registrationstart);
+      })
+  }
+  
+  
+  
+  const checkParticipants=(email,usn,callback)=>{
+      var message="";
+      var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
+     
+      ref.once('value').then(snap=>{
+          if(snap.val()!=null){
+          snap.forEach(element=>{
+              if(email===element.val().Email || usn===element.val().USN){
+                   message="already exists";
+              }
+          });
+            return callback(message);
+      }else{
+          return callback(message);
+      }
+      })
+  }
+  
+  
+  
+  app.get("/team",(req,res)=>{
+      res.render("members");
+  })
+  
+  app.get("/event",(req,res)=>{
+      res.render("event");
+  })
+  
+  app.get("/register",(req,res)=>{
+      res.render("register");
+  })
+  
+  app.post("/registerParticipants",(req,res)=>{
+      var messageback="";
+      var name=req.body.name;
+      var usn=req.body.usn;
+      var email=req.body.email;
+      var phone=req.body.phone;
+      var city=req.body.city;
+      var college=req.body.college;
+      var selected=req.body.selected;
+     
       
-       if(selected === ""){
-           res.send({message:"checkbox not selected"});
-       }else{
-
-        var date=new Date();
-
-        var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
         
-        var userkey = ref.push().key;
-
-        checkRegistrationStart((start)=>{
-            
-            if(start===1){
-                checkParticipants(email,usn,(info)=>{
-                    // console.log(info);
-                    if(info === "already exists"){
-                        messageback=info;
-                        res.send({message:messageback});
-                    }else{
-                        
-                       
-                        ref.child(userkey).set({
-                            Name:name,
-                            Email:email,
-                            USN:usn,
-                            "Phone Number":phone,
-                            City:city,
-                            College:college,
-                            "Time of Registration":date.toString(),
-                            "Date of Registration":date.toLocaleDateString(),
-                            "Opted For":selected
-                        },(error)=>{
-                            if(error){
-                                res.send({message:"error"});
-                            }else{
-                                messageback="success";
-
-                                const mailOptions = {
-                                    from: '"ScriptInk" <sender@email.com>',
-                                    to: email, 
-                                    subject: 'WritoFest 2k20',
-                                    html: `<p>Greetings ${name}<br><br>
-                                    
-                                    You have been successfully registered for WritoFest 2K20.<br><br>
-                                    Follow following instructions:<br><br>
-                                    
-                                    •Event will be hosted in a virtual environment on Cisco Webex Meetings.<br>
-                                    •You will be provided with meetings details 24hrs before the beginning of event<br>
-                                    •Writing competition and submission will be taken via our android application<br>
-                                    •You can download our app from the following link:<br>
-                                    https://play.google.com/store/apps/details?id=com.scriptink.official<br><br>
-                                    
-                                    For further information feel free to reach us anytime via following Contacts:<br><br>
-                                    
-                                    Email: reachscriptink@gmail.com<br>
-                                    Phone: +91-80950-30481<br><br>
-                                    
-                                    We are open 24x7.<br><br>
-                                    
-                                    Team Scriptink </p>`
-                                  };
-
-                                  transporter.sendMail(mailOptions, function (err, info) {
-                                    if(err)
-                                      console.log(err)
-                                    else
-                                      console.log(info);
+         if(selected === ""){
+             res.send({message:"checkbox not selected"});
+         }else{
+  
+          var date=new Date();
+  
+          var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
+          
+          var userkey = ref.push().key;
+  
+          checkRegistrationStart((start)=>{
+              
+              if(start===1){
+                  checkParticipants(email,usn,(info)=>{
+                      // console.log(info);
+                      if(info === "already exists"){
+                          messageback=info;
+                          res.send({message:messageback});
+                      }else{
+                          
+                         
+                          ref.child(userkey).set({
+                              Name:name,
+                              Email:email,
+                              USN:usn,
+                              "Phone Number":phone,
+                              City:city,
+                              College:college,
+                              "Time of Registration":date.toString(),
+                              "Date of Registration":date.toLocaleDateString(),
+                              "Opted For":selected
+                          },(error)=>{
+                              if(error){
+                                  res.send({message:"error"});
+                              }else{
+                                  messageback="success";
+  
+                                  const mailOptions = {
+                                      from: '"ScriptInk" <sender@email.com>',
+                                      to: email, 
+                                      subject: 'WritoFest 2k20',
+                                      html: `<p>Greetings ${name}<br><br>
                                       
-                                });
-
-
-                                res.send({message:messageback});
-                            }
-                        });
-
-
-                    
-                    }
-                })
-            }else{
-                messageback="not started";
-                res.send({message:messageback});
-            }
-        })
-    }
-      
-      
+                                      You have been successfully registered for WritoFest 2K20.<br>
+                                      Follow following instructions:<br><br>
+                                      
+                                      •Event will be hosted in a virtual environment on Cisco Webex Meetings.<br>
+                                      •You will be provided with meetings details 24hrs before the beginning of event<br>
+                                      •Writing competition and submission will be taken via our android application<br>
+                                      •You can download our app from the following link:<br>
+                                      https://play.google.com/store/apps/details?id=com.scriptink.official<br><br>
+                                      
+                                      For further information feel free to reach us anytime via following Contacts:<br><br>
+                                      
+                                      Email: reachscriptink@gmail.com<br>
+                                      Phone: +91-80950-30481<br><br>
+                                      
+                                      We are open 24x7.<br><br>
+                                      
+                                      Team Scriptink </p>`
+                                    };
+  
+                                    transporter.sendMail(mailOptions, function (err, info) {
+                                      if(err)
+                                        res.send({message:messageback});
+                                      else
+                                        res.send({message:messageback});
+                                  });
+                                      
+  
+  
+                                  
+                              }
+                          });
+  
+  
+                      
+                      }
+                  })
+              }else{
+                  messageback="not started";
+                  res.send({message:messageback});
+              }
+          })
+      }
         
-   
-})
-
-
-app.post("/getTeam",(req,res)=>{
-    var member=[];
-    var ref=firebase.database().ref("/Our Team");
-    ref.once("value").then(snap=>{
-        snap.forEach(element=>{
-            member.push(element.val());
-        })
-        res.send({array:member});
-    })
-})
-
-
-//category route
-app.get("/:post",function(req,res){
-
-    var posts=[];
-    var backimg=[];
-    var category=req.params.post;
-    var categoryName,caption,image,back;
-
-    if(category==="inspiration"){
-         categoryName="Inspiration";
-         caption="Grow Inspired";
-         image="srk.jpg";
-         back="inspiration.jpg";
-
-    }
-    else if(category=="love"){
-        categoryName="Love";
-        caption="A Bliss Feeling";
-        image="love.jpg";
-        back="loveback.jpg";
-    }
-    else if(category=="sad"){
-        categoryName="Sad";
-        caption="Life shows it's way";
-        image="sad1.jfif";
-        back="sadb.jpg";
-    }else if(category=="science"){
-        categoryName="Science";
-        caption="Innovate with Science";
-        image="space.jpg";
-        back="spaceb.jpg";
-    }else{
-        res.send("Error 404");
-    }
-   
-    res.render(__dirname+"/views/posts.ejs",{category:categoryName,caption:caption,image:image,back:back});
-    
-    
-
-})
-
-app.post("/getData",function(req,res){
-     var type=req.body.type;
-     var array=[];
-     var backimg=[];
-     var category=req.body.category;
-     if(category === "Science"){
-         category="science fiction"
-     }
-     var ref=firebase.database().ref("/categories/"+category.toLowerCase()+"/"+type+"/writings/english");
-     var ref1=firebase.database().ref("/categories/"+category.toLowerCase()+"/"+type+"/backimgurls/english");
-     ref.once('value').then(snap=>{
-         snap.forEach(element => {
-             array.push(element.val());
-         });
-         ref1.once('value').then(snap1=>{
-            snap1.forEach(element => {
-                backimg.push(element.val());
-            });
-            res.send({array:array,backimg:backimg});
-        });
-     })
-});
-
-
-
-app.listen(process.env.PORT||3000,function(){
-     console.log("ScriptInk Website Is live on server 3000");
-});
+        
+          
+     
+  })
+  
+  
+  app.post("/getTeam",(req,res)=>{
+      var member=[];
+      var ref=firebase.database().ref("/Our Team");
+      ref.once("value").then(snap=>{
+          snap.forEach(element=>{
+              member.push(element.val());
+          })
+          res.send({array:member});
+      })
+  })
+  
+  
+  //category route
+  app.get("/:post",function(req,res){
+  
+      var posts=[];
+      var backimg=[];
+      var category=req.params.post;
+      var categoryName,caption,image,back;
+  
+      if(category==="inspiration"){
+           categoryName="Inspiration";
+           caption="Grow Inspired";
+           image="srk.jpg";
+           back="inspiration.jpg";
+  
+      }
+      else if(category=="love"){
+          categoryName="Love";
+          caption="A Bliss Feeling";
+          image="love.jpg";
+          back="loveback.jpg";
+      }
+      else if(category=="sad"){
+          categoryName="Sad";
+          caption="Life shows it's way";
+          image="sad1.jfif";
+          back="sadb.jpg";
+      }else if(category=="science"){
+          categoryName="Science";
+          caption="Innovate with Science";
+          image="space.jpg";
+          back="spaceb.jpg";
+      }else{
+          res.send("Error 404");
+      }
+     
+      res.render(__dirname+"/views/posts.ejs",{category:categoryName,caption:caption,image:image,back:back});
+      
+      
+  
+  })
+  
+  app.post("/getData",function(req,res){
+       var type=req.body.type;
+       var array=[];
+       var backimg=[];
+       var category=req.body.category;
+       if(category === "Science"){
+           category="science fiction"
+       }
+       var ref=firebase.database().ref("/categories/"+category.toLowerCase()+"/"+type+"/writings/english");
+       var ref1=firebase.database().ref("/categories/"+category.toLowerCase()+"/"+type+"/backimgurls/english");
+       ref.once('value').then(snap=>{
+           snap.forEach(element => {
+               array.push(element.val());
+           });
+           ref1.once('value').then(snap1=>{
+              snap1.forEach(element => {
+                  backimg.push(element.val());
+              });
+              res.send({array:array,backimg:backimg});
+          });
+       })
+  });
+  
+  
+  
+  app.listen(process.env.PORT||3000,function(){
+       console.log("ScriptInk Website Is live on server 3000");
+  });
+  
