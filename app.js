9b39caac518c99f4a6d4c27b65db22c89f32f323
AnuragCool <anuragcooldavkh@gmail.com>
2020-12-23 18:27:39 +0530

    registration details route added

--- app.js
+++ app.js
@@ -43,6 +43,95 @@ var firebaseConfig = {
   app.get("/",function(req,res){
       res.render("landing");
   })
+
+
+  
+  var count1=0;
+  var count2=0;
+  var count3=0;
+  var count4=0;
+  var count5=0;
+  var count6=0;
+  var count7=0;
+  var count8=0;
+  var count9=0;
+  var counts=0;
+  var count16=0;
+  var count17=0;
+  var count18=0;
+  var count19=0;
+  var count=0;
+  var participants;
+
+  app.get("/registrationDetails",(req,res)=>{
+    participants=[];
+    var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
+     
+      ref.once('value').then(snap=>{
+          if(snap.val()!=null){
+          snap.forEach(element=>{
+            count++;
+            participants.push(element.val());
+              
+              if(element.val().College.includes("Siddaganga") || element.val().College.includes("SIT") || element.val().College.includes("siddaganga") || element.val().College.includes("sit") || element.val().College.includes("Sit") || element.val().College.includes("SIDDAGANGA")){
+                 counts++;
+              }
+              if(element.val().USN.includes("cs")||element.val().USN.includes("CS")){
+                count1++;
+              }
+              if(element.val().USN.includes("is")||element.val().USN.includes("IS")){
+                count2++;
+             }
+             if(element.val().USN.includes("ec")||element.val().USN.includes("EC")){
+                count3++;
+             }
+             if(element.val().USN.includes("ee")||element.val().USN.includes("EE")){
+                count4++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("TE")){
+                count5++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("ET")){
+                count6++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("EI")){
+                count7++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("ME")){
+                count8++;
+             }
+            if(element.val().USN.includes("ch")||element.val().USN.includes("CH")){
+                count9++;
+             }
+
+
+            if(element.val().USN.toLowerCase().includes("1si16")){
+                count16++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si17")){
+                count17++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si18")){
+                count18++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si19")){
+                count19++;
+            }
+
+          });
+        //   console.log(count1 +" "+ count2+" "+ count3+" "+ count4+" "+ count5+" "+ count6+" "+ count7+" "+ count8+" ch: "+count9 );
+        //   console.log("Total :"+counts+"  "+count);
+        //   console.log("16- "+count16+" 17- "+count17+" 18- "+count18+" 19- "+count19);
+          participants.push({"Total : ":count});
+        //   console.log(participants);
+
+          res.send(participants);
+         
+
+      }
+      })
+
+  })
   
   
   
@@ -59,8 +148,7 @@ var firebaseConfig = {
       })
   }
   
-  
-  
+
   const checkParticipants=(email,usn,callback)=>{
       var message="";
       var ref = firebase.database().ref("/WritoFest/Registrations/WritoFest2020/ViaWebsite");
@@ -68,10 +156,60 @@ var firebaseConfig = {
       ref.once('value').then(snap=>{
           if(snap.val()!=null){
           snap.forEach(element=>{
+            count++;
               if(email===element.val().Email || usn===element.val().USN){
                    message="already exists";
               }
+              if(element.val().College.includes("Siddaganga") || element.val().College.includes("SIT") || element.val().College.includes("siddaganga") || element.val().College.includes("sit") || element.val().College.includes("Sit") || element.val().College.includes("SIDDAGANGA")){
+                 counts++;
+              }
+              if(element.val().USN.includes("cs")||element.val().USN.includes("CS")){
+                count1++;
+              }
+              if(element.val().USN.includes("is")||element.val().USN.includes("IS")){
+                count2++;
+             }
+             if(element.val().USN.includes("ec")||element.val().USN.includes("EC")){
+                count3++;
+             }
+             if(element.val().USN.includes("ee")||element.val().USN.includes("EE")){
+                count4++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("TE")){
+                count5++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("ET")){
+                count6++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("EI")){
+                count7++;
+             }
+             if(element.val().USN.includes("te")||element.val().USN.includes("ME")){
+                count8++;
+             }
+            if(element.val().USN.includes("ch")||element.val().USN.includes("CH")){
+                count9++;
+             }
+
+
+            if(element.val().USN.toLowerCase().includes("1si16")){
+                count16++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si17")){
+                count17++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si18")){
+                count18++;
+            }
+            if(element.val().USN.toLowerCase().includes("1si19")){
+                count19++;
+            }
+
           });
+          console.log(count1 +" "+ count2+" "+ count3+" "+ count4+" "+ count5+" "+ count6+" "+ count7+" "+ count8+" ch: "+count9 );
+          console.log("Total :"+counts+"  "+count);
+          console.log("16- "+count16+" 17- "+count17+" 18- "+count18+" 19- "+count19);
+
             return callback(message);
       }else{
           return callback(message);
